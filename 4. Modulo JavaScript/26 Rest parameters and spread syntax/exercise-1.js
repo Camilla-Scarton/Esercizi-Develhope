abc61cf781bc4964ec6082d2a5c8ca439a8a7cd6
Camilla Scarton <camy.s96@gmail.com>
2022-11-10 15:47:25 +0100

    done exercise 26

--- 4. Modulo JavaScript/26 Rest parameters and spread syntax/exercise-1.js
+++ 4. Modulo JavaScript/26 Rest parameters and spread syntax/exercise-1.js
@@ -1,5 +1,10 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+// Codice da modificare
+// function sum(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+
+function sum(...numbs) { // così si crea un array: numbs = [1, 2, 3, 4, 5] 
+    return numbs.reduce((a,b) => a+b); // e si sfruttano i metodi degli array
 }
 
-console.log(sum(1, 2, 3, 4, 5));
+console.log(sum(1, 2, 3, 4, 5)); // deve stampare 15
