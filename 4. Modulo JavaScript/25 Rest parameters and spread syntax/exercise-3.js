a934e243a15339d7430c8a84e56fdb6a0ba1eb42
Camilla Scarton <camy.s96@gmail.com>
2022-11-10 15:40:31 +0100

    done exercise 25

--- 4. Modulo JavaScript/25 Rest parameters and spread syntax/exercise-3.js
+++ 4. Modulo JavaScript/25 Rest parameters and spread syntax/exercise-3.js
@@ -1,4 +1,7 @@
 let numberStore = [0, 1, 2];
 let newNumber = 3;
-numberStore.push(newNumber);
-console.log(numberStore);
+
+// numberStore.push(newNumber); diventa...
+numberStore = [...numberStore, newNumber];
+
+console.log(numberStore); // deve stampare [0, 1, 2, 3]
