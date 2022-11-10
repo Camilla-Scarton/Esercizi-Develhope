b098c19401cb6869a6c0ed2360204533c74b67c3
Camilla Scarton <camy.s96@gmail.com>
2022-11-10 15:34:03 +0100

    done exercise 23

--- 4. Modulo JavaScript/23 Destructuring/exercise-3.js
+++ 4. Modulo JavaScript/23 Destructuring/exercise-3.js
@@ -1,5 +1,11 @@
-function isAdult(person) {
-  return person.age >= 18;
+// Codice da modificare
+// function isAdult(person) {
+//   return person.age >= 18;
+// }
+
+// L'oggetto in input è subito destrutturato, così da usare direttamente age a riga 8
+function isAdult({id, firstName, lastName, age}) {
+  return age >= 18;
 }
 
 const person = {
