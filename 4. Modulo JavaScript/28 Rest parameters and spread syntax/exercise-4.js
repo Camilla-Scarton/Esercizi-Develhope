90e0e90be921b9935bff716b42262bff1b2776b3
Camilla Scarton <camy.s96@gmail.com>
2022-11-10 17:03:27 +0100

    done exercise 28

--- 4. Modulo JavaScript/28 Rest parameters and spread syntax/exercise-4.js
+++ 4. Modulo JavaScript/28 Rest parameters and spread syntax/exercise-4.js
@@ -1,6 +1,13 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+// function sum(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+
+// const numbers = [1, 2, 3];
+// console.log(sum(numbers[0], numbers[1], numbers[2])); stampa 6
+
+function sum(...numbs) {
+  return numbs.reduce((a,b) => a+b);
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(...numbers));
