e6a30ead55af57649af4306a02a5eee9cc3ec084
Camilla Scarton <camy.s96@gmail.com>
2022-11-13 21:02:23 +0100

    correction exercise 6

--- 4. Modulo JavaScript/06 Closures/exercise.js
+++ 4. Modulo JavaScript/06 Closures/exercise.js
@@ -1,9 +1,21 @@
+// Solzuione funzionante, con inner() chiamata all'esterno della funzione
+// function printName() {
+//     let helloName = "Hello John";
+//     function inner() {
+//         return helloName;
+//     }
+//     return inner;
+// }
+
+// console.log(printName()());
+
+// Solzuione funzionante, con inner() chiamata dentro la funzione
 function printName() {
     let helloName = "Hello John";
     function inner() {
         return helloName;
     }
-    return inner;
+    return inner();
 }
 
-console.log(printName()());
+console.log(printName());
