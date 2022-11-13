95170dc0f9263f552c9cc69eb06444ce29f8ec6e
Camilla Scarton <camy.s96@gmail.com>
2022-11-13 21:08:07 +0100

    correction exercise 9

--- 4. Modulo JavaScript/09 Objects methods/exercise-1.js
+++ 4. Modulo JavaScript/09 Objects methods/exercise-1.js
@@ -4,9 +4,17 @@ const person = {
   age: 25
 }
 
-const personKeys = Object.keys(person);
-const personValues = Object.values(person);
+// Soluzione con Object.keys/values
+// const personKeys = Object.keys(person);
+// const personValues = Object.values(person);
 
-for (let i = 0; i < personKeys.length; i++) {
-  console.log(personKeys[i] + ": " + personValues[i]);
+// for (let i = 0; i < personKeys.length; i++) {
+//   console.log(personKeys[i] + ": " + personValues[i]);
+// }
+
+// Soluzione con Object.entries
+const personEntries = Object.entries(person);
+
+for (let i = 0; i < personEntries.length; i++) {
+  console.log(personEntries[i][0] + ": " + personEntries[i][1]);
 }
