358a207feded921e2d93243b3bb91607a5569ca7
Camilla Scarton <camy.s96@gmail.com>
2022-11-13 23:02:35 +0100

    corrected exercise 4 with correct math formula

--- 4. Modulo JavaScript/04 Conditional and loops/exercise-7.js
+++ 4. Modulo JavaScript/04 Conditional and loops/exercise-7.js
@@ -1,28 +1,34 @@
 function sumUntil(maxValue) {
-  let sum = 0;
-  if (maxValue > 0) {
-    while (maxValue > 0) {
-      sum = sum + maxValue;
-      maxValue--;
-    }
-    /*
-    let i = 1;
-    while (i <= maxValue) {
-      sum = sum + i;
-      i++;
-    }
-    */
-    /*
-    for (let i = 0; i <= maxValue; i++) {
-      sum = sum + i;
-    }
-    */
-    return sum;
-  } else if (maxValue == 0) {
-    return sum;
-  } else if (maxValue < 0) {
-    return "Only positive numbers in input!";
+  if (maxValue >= 0) {
+    return maxValue * (maxValue + 1) / 2;
+  } else {
+    return -(maxValue * (maxValue - 1) / 2);
   }
+
+  // let sum = 0;
+  // if (maxValue > 0) {
+  //   while (maxValue > 0) {
+  //     sum = sum + maxValue;
+  //     maxValue--;
+  //   }
+  //   /*
+  //   let i = 1;
+  //   while (i <= maxValue) {
+  //     sum = sum + i;
+  //     i++;
+  //   }
+  //   */
+  //   /*
+  //   for (let i = 0; i <= maxValue; i++) {
+  //     sum = sum + i;
+  //   }
+  //   */
+  //   return sum;
+  // } else if (maxValue == 0) {
+  //   return sum;
+  // } else if (maxValue < 0) {
+  //   return "Only positive numbers in input!";
+  // }
 }
 
 console.log(sumUntil(5));
