e1c3f783380babffd486402135ddb4c12b82fba7
Camilla Scarton <camy.s96@gmail.com>
2022-11-16 17:19:06 +0100

    completed exercise 33

--- 4. Modulo JavaScript/33 Template strings/exercise.js
+++ 4. Modulo JavaScript/33 Template strings/exercise.js
@@ -6,4 +6,4 @@ class Person {
 }
 
 const developer = new Person("Mario", "Rossi");
-console.log(developer.firstName + " " + developer.lastName);
+console.log(`${developer.firstName} ${developer.lastName}`);
