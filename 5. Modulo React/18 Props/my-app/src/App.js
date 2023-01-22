7bb1f6a61b3c340b180b4a1dab5305300125e83d
Camilla Scarton <camy.s96@gmail.com>
2023-01-22 13:40:21 +0100

    corrected comment for Alessandro in exercise 18

--- 5. Modulo React/18 Props/my-app/src/App.js
+++ 5. Modulo React/18 Props/my-app/src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Welcome } from "./Welcome"
 
 export class App extends React.Component {
-  // It works fine if we pass JSX elements as props: in this case the name is rendered bold
+  // It works fine if we pass JSX elements as props... just remember to use curly braces!
   render() {
     return <Welcome name={<strong>Camilla</strong>}/>
   }
