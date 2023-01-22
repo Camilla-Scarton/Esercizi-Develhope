288e3a055e90e6b417a3b8f3680247e4b21f5352
Camilla Scarton <camy.s96@gmail.com>
2023-01-22 13:52:01 +0100

    added comments for Alessandro in exercise 19

--- 5. Modulo React/19 Props/my-app/src/index.js
+++ 5. Modulo React/19 Props/my-app/src/index.js
@@ -1,4 +1,8 @@
 import { App } from "./App"
 import ReactDOM from "react-dom"
 
+// Tree: App -> Welcome -> Age
+// App is Welcome's parent. Welcome is App's child and Age's parent. Age is Welcome's child.
+// App gets the props values. 
+// The props values are passed from parent to child component with "this.props.name" or "this.props.age" as many times as necessary.
 ReactDOM.render(<App name="Camilla" age={26}/>, document.querySelector("#root"))
