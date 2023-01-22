029ecb06a4823d216ec8e367e944f409e152a565
Camilla Scarton <camy.s96@gmail.com>
2023-01-22 13:33:17 +0100

    added comment for Alessandro in exercise 18

--- 5. Modulo React/18 Props/my-app/src/App.js
+++ 5. Modulo React/18 Props/my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Welcome } from "./Welcome"
 
 export class App extends React.Component {
+  // It works fine if we pass JSX elements as props: in this case the name is rendered bold
   render() {
     return <Welcome name={<strong>Camilla</strong>}/>
   }
