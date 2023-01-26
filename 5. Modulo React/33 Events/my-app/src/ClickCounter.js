2cde54d090386bb0a854d5a789218c0bc2fabcab
Camilla Scarton <camy.s96@gmail.com>
2023-01-26 18:34:27 +0100

    corrected and formatted exercise 33

--- 5. Modulo React/33 Events/my-app/src/ClickCounter.js
+++ 5. Modulo React/33 Events/my-app/src/ClickCounter.js
@@ -1,24 +1,22 @@
-import React from "react";
+import { Component } from "react";
 
-export class ClickCounter extends React.Component {
-    state = {
-        counter: 0,
-    }
+export class ClickCounter extends Component {
+  state = {
+    counter: 0,
+  };
 
-    incrementOnClick = () => {
-        this.setState(state => {
-            return {
-                counter: this.state.counter + 1,
-            }
-        })
-    }
-    
-    render() {
-        return (
-            <div>
-                <div>Il counter è a: {this.state.counter}</div>
-                <button onClick={this.incrementOnClick}>Button</button>
-            </div>
-        )
-    }
+  incrementOnClick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div>Il counter è a: {this.state.counter}</div>
+        <button onClick={this.incrementOnClick}>Button</button>
+      </>
+    );
+  }
 }
