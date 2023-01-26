a5534785ad9311082c078f3d26f9112a2b554fa4
Camilla Scarton <camy.s96@gmail.com>
2023-01-26 18:28:54 +0100

    corrected and formatted exercise 30

--- 5. Modulo React/30 Component Lifecycle/my-app/src/Counter.js
+++ 5. Modulo React/30 Component Lifecycle/my-app/src/Counter.js
@@ -1,28 +1,26 @@
-import React from "react"
+import { Component } from "react";
 
-export class Counter extends React.Component {
-    state = {
-        count: this.props.initialCount,
-    }
+export class Counter extends Component {
+  state = {
+    count: this.props.initialCount,
+  };
 
-    // constructor is not required
-    componentDidMount() {
-        setInterval(() => {
-            this.setState((state) => {
-                return { 
-                    count: this.state.count + this.props.increment
-                }
-            })
-        }, this.props.interval)
-    }
+  // constructor is not required
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + this.props.increment,
+      });
+    }, this.props.interval);
+  }
 
-    render() {
-        return <h1>{this.state.count}</h1>
-    }
+  render() {
+    return <h1>{this.state.count}</h1>;
+  }
 }
 
 Counter.defaultProps = {
-    initialCount: 0,
-    increment: 1,
-    interval: 1000,
-}
+  initialCount: 0,
+  increment: 1,
+  interval: 1000,
+};
