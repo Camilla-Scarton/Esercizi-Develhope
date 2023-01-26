5e6b96c757a8bc861a7d34320817446c2ef623bb
Camilla Scarton <camy.s96@gmail.com>
2023-01-26 18:20:31 +0100

    formatted file

--- 5. Modulo React/28 State/my-app/src/Counter.js
+++ 5. Modulo React/28 State/my-app/src/Counter.js
@@ -1,29 +1,27 @@
-import React from "react"
-import { CounterDisplay } from "./CounterDisplay"
+import { Component } from "react";
+import { CounterDisplay } from "./CounterDisplay";
 
-export class Counter extends React.Component {
-    constructor(props) {
-		super(props)
-		this.state = {
-			count: this.props.initialCount,
-		}
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.initialCount,
+    };
 
-        setInterval(() => {
-            this.setState((state) => {
-                return { 
-                    count: this.state.count + this.props.increment
-                }
-            })
-        }, this.props.interval)
-	}
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + this.props.increment,
+      });
+    }, this.props.interval);
+  }
 
-    render() {
-        return <CounterDisplay count={this.state.count} />
-    }
+  render() {
+    return <CounterDisplay count={this.state.count} />;
+  }
 }
 
 Counter.defaultProps = {
-    initialCount: 0,
-    increment: 1,
-    interval: 1000,
-}
+  initialCount: 0,
+  increment: 1,
+  interval: 1000,
+};
