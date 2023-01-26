297c73cf1b8530909aa514593465bba6b816791d
Camilla Scarton <camy.s96@gmail.com>
2023-01-26 18:39:09 +0100

    corrected and formatted esercise 35

--- 5. Modulo React/35 Events/my-app/src/ClickTracker.js
+++ 5. Modulo React/35 Events/my-app/src/ClickTracker.js
@@ -1,26 +1,30 @@
-import React from "react";
+import { Component } from "react";
 
-export class ClickTracker extends React.Component {
-    state = {
-        lastClicked: "No one",
-    }
+export class ClickTracker extends Component {
+  state = {
+    lastClicked: "No one",
+  };
 
-    lastClicked = (event) => {
-        this.setState(state => {
-            return {
-                lastClicked: event.target.name,
-            }
-        }) 
-    }
+  lastClicked = (event) => {
+    this.setState({
+      lastClicked: event.target.name,
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <button name="Button 1" onClick={this.lastClicked} >Button 1</button>
-                <button name="Button 2" onClick={this.lastClicked} >Button 2</button>
-                <button name="Button 3" onClick={this.lastClicked} >Button 3</button>
-                <h1>The last button clicked is: {this.state.lastClicked}</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <>
+        <button name="Button 1" onClick={this.lastClicked}>
+          Button 1
+        </button>
+        <button name="Button 2" onClick={this.lastClicked}>
+          Button 2
+        </button>
+        <button name="Button 3" onClick={this.lastClicked}>
+          Button 3
+        </button>
+        <h1>The last button clicked is: {this.state.lastClicked}</h1>
+      </>
+    );
+  }
 }
