a2583be37e21fd6b5fbdf7037bb96038e05b0b75
Camilla Scarton <camy.s96@gmail.com>
2023-01-27 18:21:35 +0100

    done exercise 57, for real this time :D

---
+++ 5. Modulo React/57 Render props/my-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { TodoList } from "./TodoList";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <TodoList>
+    {(items, deleteItem) => {
+      return (
+        items.map((todo,i) => {
+          return (
+            <>
+              <li key={i}>{todo}</li>
+              <button onClick={() => {deleteItem(i)}}>Remove item</button>
+            </>
+          )
+        })
+      )
+    }}
+  </TodoList>
+);
+
