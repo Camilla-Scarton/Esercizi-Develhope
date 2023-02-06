a60afab38a18db245651c39685e9a2cbc656ff66
Camilla Scarton <camy.s96@gmail.com>
2023-02-06 23:47:04 +0100

    done exercises 58-59

--- 5. Modulo React/58 Context/my-app/src/index.js
+++ 5. Modulo React/58 Context/my-app/src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { LanguageContext } from './LanguageContext';
-import { DisplayLanguage } from "./DisplayLanguage";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
-  <><div>ciao</div></>
-
-  
+  <App />
 );
-
-/*
-<LanguageContext.Provider>
-    <DisplayLanguage />
-  </LanguageContext.Provider>
-*/
--- 5. Modulo React/59 Context/my-app/src/App.js
+++ 5. Modulo React/59 Context/my-app/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { LanguageContext } from "./LanguageContext";
+import { DisplayLanguage } from "./DisplayLanguage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+export class App extends Component {
+  state = {
+    language: "English",
+  };
+
+  handleSelect = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <select value={this.state.language} onChange={this.handleSelect}>
+          <option value="English">English</option>
+          <option value="Italiano">Italiano</option>
+        </select>
+        <LanguageContext.Provider value={this.state.language}>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The language shown comes from the "value" attribute in the select tag.
+          <br />
+          The "value" attribute value is saved in the state and then passed to the provider.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </>
+    );
+  }
 }
-
-export default App;
