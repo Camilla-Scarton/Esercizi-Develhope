1a752fb5165c650fb384662d0ab476e0a491d049
Camilla Scarton <camy.s96@gmail.com>
2023-02-09 18:26:35 +0100

    done exercises 01 ts

--- 7. Modulo TypeScript/01 Primitive Types/exercise-1-primitive-types-start.ts
+++ 7. Modulo TypeScript/01 Primitive Types/exercise-1-primitive-types-start.ts
@@ -4,23 +4,23 @@
 
 // Add type annotations to each of these variables.
 
-let country = "Italy";
+let country: string = "Italy";
 
-let year = undefined;
+let year: undefined = undefined;
 
-let averageTemperature = 23.6;
+let averageTemperature: number = 23.6;
 
-let visited = true;
+let visited: boolean = true;
 
-let currency = null;
+let currency: null = null;
 
 // Change the type annotations on these variables so they are correct.
 
-let population: boolean = 47_450_795;
+let population: number = 47_450_795;
 
-let isSummer: string = false;
+let isSummer: boolean = false;
 
-let languages: number = "Spanish, English";
+let languages: string = "Spanish, English";
 
 // ----
 
