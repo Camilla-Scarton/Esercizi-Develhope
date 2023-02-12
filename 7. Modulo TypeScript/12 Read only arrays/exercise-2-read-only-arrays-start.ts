2b7c7583db3c753821f9142b28011d3066c76b99
Camilla Scarton <camy.s96@gmail.com>
2023-02-12 17:27:59 +0100

    done exercise 12 ts

--- 7. Modulo TypeScript/12 Read only arrays/exercise-2-read-only-arrays-start.ts
+++ 7. Modulo TypeScript/12 Read only arrays/exercise-2-read-only-arrays-start.ts
@@ -6,9 +6,9 @@
 // Use the generic interface type `ReadonlyArray`: ReadonlyArray<type>
 // This will cause a type error. Remove the code that now has a type error.
 
-const languages = ["Mongolian", "French", "Basque", "Thai"];
+const languages: ReadonlyArray<string> = ["Mongolian", "French", "Basque", "Thai"];
 
-languages[2] = "Hausa";
+//languages[2] = "Hausa"; <- Wrong code because languages is now readonly.
 
 console.log(languages);
 
@@ -16,9 +16,9 @@ console.log(languages);
 // Use the readonly modifier and the array literal type syntax: readonly type[]
 // This will cause a type error. Remove the code that now has a type error.
 
-const currencies = ["NZD", "THB", "NGN", "EUR", "COP", "INR"];
+const currencies: readonly string[] = ["NZD", "THB", "NGN", "EUR", "COP", "INR"];
 
-currencies.reverse();
+// currencies.reverse(); <- Wrong code because currencies is now readonly.
 
 console.log(currencies);
 
