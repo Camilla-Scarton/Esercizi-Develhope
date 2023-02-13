10ecf0c82cab902c0bdcc9d7a58b9ba04cb23652
Camilla Scarton <camy.s96@gmail.com>
2023-02-13 14:55:15 +0100

    done exercise 22 ts

--- 7. Modulo TypeScript/22 Construct types with Record/exercise-2-construct-types-with-record-start.ts
+++ 7. Modulo TypeScript/22 Construct types with Record/exercise-2-construct-types-with-record-start.ts
@@ -4,7 +4,7 @@
 
 // Fix this use of the `Record` utility type so it constructs a valid type.
 
-const currency: Record<> = {
+const currency: Record<string, string> = {
     name: "United States dollar",
     code: "USD",
     symbol: "$",
@@ -20,7 +20,8 @@ interface Currency {
     symbol: string;
 }
 
-const currencies = {
+// Also valid: Record<"france" | "china" | "colombia", Currency>
+const currencies: Record<string, Currency> = {
     france: {
         name: "Euro",
         code: "EUR",
