9529419cf41931b0483f5b71b139dd2eda4f546b
Camilla Scarton <camy.s96@gmail.com>
2023-02-13 12:23:14 +0100

    done exercise 18 ts

--- 7. Modulo TypeScript/18 Add union types/exercise-1-add-union-types-start.ts
+++ 7. Modulo TypeScript/18 Add union types/exercise-1-add-union-types-start.ts
@@ -8,7 +8,7 @@
 //
 // This should fix the type error on Line 15.
 
-type Currency = string;
+type Currency = string | boolean;
 
 let indianCurrency: Currency = "Indian rupee";
 
@@ -19,7 +19,7 @@ let narniaCurrency: Currency = false;
 //
 // This should fix the type error on Lines 28 and 30.
 
-function outputCountryData(country: string, data: number) {
+function outputCountryData(country: string, data: number | string) {
     console.log(`The data for ${country} is: ${data}`);
 }
 
