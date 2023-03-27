044f6683a71422f27180f119de2fb519d75873c2
Camilla Scarton <114734186+Camilla-Scarton@users.noreply.github.com>
2023-03-27 19:54:43 +0200

    Update server.ts

--- 8. Modulo Node.js/11 Set up a simple Express App/src/server.ts
+++ 8. Modulo Node.js/11 Set up a simple Express App/src/server.ts
@@ -9,6 +9,25 @@ dotenv.config();
 app.use(morgan("dev")); // to log client's request
 app.use(express.json()); // to configure the express app to accept json
 
+// simple database
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
 app.listen(process.env.PORT, () =>
   console.log(`app listenning to port http://localhost:${process.env.PORT}`)
 );
